da34b03ab9e63267878d7a7cc7a9eb733b4b5cae
CryptoDevFr <romain.bergery@gmail.com>
2020-08-26 17:34:01 +0200

    up

--- electrumx/lib/coins.py
+++ electrumx/lib/coins.py
@@ -2688,6 +2688,42 @@ class GroestlcoinTestnet(Groestlcoin):
         'aocojvqcybdoxekv.onion t',
     ]
 
+class Helix(Coin):
+    NAME = "Helix"
+    SHORTNAME = "HLIX"
+    NET = "mainnet"
+    XPUB_VERBYTES = bytes.fromhex("022D2533")
+    XPRV_VERBYTES = bytes.fromhex("0221312B")
+    GENESIS_HASH = '79a3c45d6e2760efb4d6de76d34b1f4833ba919bc114e1da0f671b1700a78f08'
+    P2PKH_VERBYTE = bytes.fromhex("28")
+    P2SH_VERBYTE = bytes.fromhex("0d")
+    WIF_BYTE = bytes.fromhex("d4")
+    TX_COUNT_HEIGHT = 569399
+    TX_COUNT = 2157510
+    TX_PER_BLOCK = 1
+    STATIC_BLOCK_HEADERS = False
+    RPC_PORT = 37416
+    ZEROCOIN_HEADER = 112
+    ZEROCOIN_START_HEIGHT = 90201
+    ZEROCOIN_BLOCK_VERSION = 4
+
+    @classmethod
+    def static_header_len(cls, height):
+        '''Given a header height return its length.'''
+        if height >= cls.ZEROCOIN_START_HEIGHT:
+            return cls.ZEROCOIN_HEADER
+        else:
+            return cls.BASIC_HEADER_SIZE
+
+    @classmethod
+    def header_hash(cls, header):
+        '''Given a header return the hash.'''
+        version, = struct.unpack('<I', header[:4])
+        if version >= cls.ZEROCOIN_BLOCK_VERSION:
+            return super().header_hash(header)
+        else:
+            import quark_hash
+            return quark_hash.getPoWHash(header)
 
 class Pivx(Coin):
     NAME = "PIVX"
